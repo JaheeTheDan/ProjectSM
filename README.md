bd6c1dac6cc219afad1c0f5571fbd54e612b56e8
JaheeTheDan <jaheemshaw2@outlook.com>
2022-11-18 17:45:11 -0500

    Inital code uploaded

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project SM
+
+'SM' stand for Shock Me, without context it make no sence. But it's a bad idea coming to reality.
+
+It a project that shock the user (or the masochist) that will be wearing a shock collor. The shock request will send through a website and to rasberry Pi (or whatever device is used.)
+
+Some form of electrical engineering, the shock collor's remote connect to the rasberry Pi(or whatever device is used.) with a relay.
+
+And for everyone who will be making this. Please try to be safe.
+
+
+Python is used for the backend and control of the shock collor.
+
+> ps. The project is not currently completed as of yet, you can look at progress in the steps.txt file.
---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, render_template
+import sounddevice as sd
+from time import sleep
+from scipy.io.wavfile import write
+
+
+def shock_me(): 
+    '''Function that shock user (not completed) '''
+    fs = 44100  # Sample rate
+    seconds = 6  # Duration of recording
+
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sleep(1)
+    print('shock')
+    sd.wait()
+
+
+    sd.play(myrecording, fs)
+    sd.wait()
+    write('static/output.wav', fs, myrecording)
+
+def wait():
+    '''Simulate a shock by waiting and'''
+    print('Shock')
+    sleep(1)
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    '''Main page'''
+    return render_template('index.html')
+
+@app.route('/button', methods=['GET', 'POST'])
+def button():
+    '''Button page which show the button that is link to audio_output page'''
+    if request.method == 'POST':
+        wait()
+    return render_template('button.html')
+
+@app.route('/audio_output', methods=['GET', 'POST'])
+def audio_output():
+    '''The end page which show the audio output'''
+    if request.method == 'POST':
+        wait()
+    return render_template('audio_output.html')
+
+app.run(debug=True)
